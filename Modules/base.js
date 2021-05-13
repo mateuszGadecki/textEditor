ac059d62603fc7badad40db50bd01f0fdb37853e
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-05-13 20:23:37 +0200

    Adding a module with DOMstrings

---
+++ Modules/base.js
@@ -0,0 +1,11 @@
+export const DOMstrings = {
+  board: document.querySelector(".board__textarea"),
+  boldBtn: document.querySelector(".js__btn_1"),
+  italicBtn: document.querySelector(".js__btn_2"),
+  bulletsBtn: document.querySelector(".js__btn_3"),
+  downloadBtn: document.querySelector(".fileHandlers__button"),
+  fileInput: document.querySelector(".fileHandlers__input"),
+  realInputBtn: document.querySelector(".fileHandlers__input"),
+  customBtn: document.querySelector(".fileHandlers__customInputBtn"),
+  customText: document.querySelector(".fileHandlers__customInputText"),
+};
